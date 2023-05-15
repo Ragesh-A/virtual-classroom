4f2bf8187c92739d87e0f11f853e80ea3930b1af
Ragesh-A <ragesha2017@gmail.com>
2023-05-15 12:02:50 +0530

    tailwind css setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        darkPrimary : "#4848BF",
+        primary : '#6060D8',
+        lightPrimary : '#949AF2',
+        textColor : '#474747',
+        lightPink : '#FBF6FA',
+        lightBlue : '#E3F0F8',
+        shadow : '#BCCBFF',
+      },boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      }
+    },
+  },
+  plugins: [],
+}
+
