b603fb57f1204a7f7e6ad71a68895f699e289d79
Ragesh-A <ragesha2017@gmail.com>
2023-05-18 14:57:37 +0530

    eslint config

--- src/components/Auth/AuthLayout.jsx
+++ src/components/Auth/AuthLayout.jsx
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router";
-import bg from '../../assets/images/authLayoutBg.png'
+import { Outlet } from 'react-router';
+import bg from '../../assets/images/authLayoutBg.png';
 
-const AuthLayout = ()=>{
+function AuthLayout() {
   return (
-  <div className="h-[100vh] relative from-darkPrimary to-primary overflow-hidden max-w-[1920px] max-h-[1080px] m-auto rounded-md p-10 bg-gradient-to-b ">
-      <img src={bg} className="w-[1920px] h-full absolute top-0 left-0" alt="background"/>
+    <div className="h-[100vh] relative from-darkPrimary to-primary overflow-hidden max-w-[1920px] max-h-[1080px] m-auto rounded-md p-10 bg-gradient-to-b ">
+      <img src={bg} className="w-[1920px] h-full absolute top-0 left-0" alt="background" />
       <Outlet />
     </div>
-    )
+  );
 }
 
 export default AuthLayout;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { RouterProvider } from 'react-router';
-import './index.css'
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // eslint-disable-next-line react/jsx-filename-extension
   <React.StrictMode>
-    <RouterProvider router={App}/>
-  </React.StrictMode>
+    <RouterProvider router={App} />
+  </React.StrictMode>,
 );
-
