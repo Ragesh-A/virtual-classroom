6af4500eec50ac160f0341a3e51ddc0cd2232515
Ragesh-A <ragesha2017@gmail.com>
2023-05-24 19:05:13 +0530

    scrollbar issue

--- src/components/header/ClassroomHeaderComponent.jsx
+++ src/components/header/ClassroomHeaderComponent.jsx
@@ -4,27 +4,27 @@ import { useState } from 'react';
 
 const ClassroomHeaderComponent = () => {
   const [popIsVisible, setPopIsVisible] = useState(false);
-  
+
   const popHandle = () => {
   setPopIsVisible(popIsVisible ? false : true);
   };
-  
+
   return (
   <>
     <ul className="flex items-center">
-      <NavLink to="/" className="border-t-4 border-transparent hover:border-white text-white font-bold p-[15px] me-1 nav">
+      <NavLink to="/" className="border-4 border-transparent hover:border-t-white text-white font-bold p-[15px] me-1 nav">
         <i className="fa-solid fa-people-roof sm:hidden"></i>
         <li className="hidden sm:block">My class</li>
       </NavLink>
       <li>
-        <button className="border-t-4 border-transparent hover:border-white text-white font-bold p-[15px] me-1 nav"
+        <button className="border-4 border-transparent hover:border-t-white text-white font-bold p-[15px] me-1 nav"
           onClick={popHandle}>
           <i className="fa-solid fa-plus sm:hidden"></i>
           <span className="hidden sm:block">Add | Join</span>
         </button>
       </li>
       <NavLink to="/meetup"
-        className="border-t-4 border-transparent hover:border-white text-white font-bold p-[15px] me-1 nav">
+        className="border-4 border-transparent hover:border-t-white text-white font-bold p-[15px] me-1 nav">
         <i className="fa-solid fa-video sm:hidden"></i>
         <li className="hidden sm:block">Meet Up</li>
       </NavLink>
