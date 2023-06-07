f1d5ba31a1199f230110d3e321ed7f5105bcf8b9
Ragesh-A <ragesha2017@gmail.com>
2023-06-07 12:12:49 +0530

    header responsive

--- src/components/classroom/header/Header.jsx
+++ src/components/classroom/header/Header.jsx
@@ -26,12 +26,12 @@ const Header = ({ page, isLecture = false }) => {
 
   return (
     <header className="flex justify-between pt-8 md:ps-16 pb-10 rounded-bl-xl">
-      <div className="max-w-[4rem] flex items-center">
+      <div className="max-w-[4rem] flex items-center ms-4 md:ms-0">
         <Link to="/home">
           <img src={logo} alt="logo" />
         </Link>
       </div>
-      <div className="flex fixed bottom-0 left-0 w-full z-50 bg-gradient-to-r from-lightPrimary to-primary items-center justify-between overflow-hidden md:relative md:rounded-s-[2rem] ps-8 md:w-[70vw] max-w-[55rem]">
+      <div className="flex fixed bottom-0 left-0 w-full z-50 bg-gradient-to-r from-lightPrimary to-primary items-center justify-between overflow-hidden md:relative md:rounded-s-[2rem] sm:ps-8 md:w-[70vw] max-w-[55rem]">
         {element}
         <div className="flex items-center my-2">
         {logged? page !== 'home' ? <Link className="text-primary font-bold bg-white p-3 px-6 ps-8 rounded-s-[2rem] uppercase" to='/profile'>
--- src/components/classroom/header/SingleClassHeader.jsx
+++ src/components/classroom/header/SingleClassHeader.jsx
@@ -24,19 +24,19 @@ const SingleClassHeaderComponent = () => {
   };
 
   return (
-    <ul className="flex justify-evenly items-center w-full">
+    <ul className="flex justify-around items-center w-full">
       <NavLink
         to="/"
         className="border-4 border-transparent hover:border-t-white text-white font-bold p-[15px] me-1 nav"
       >
-        <i className="ri-home-2-fill md:hidden text-2xl"></i>
+        <i className="ri-home-2-fill md:hidden text-xl"></i>
         <li className="hidden md:block">My class</li>
       </NavLink>
       <NavLink
         to={`/class/${classId}/`}
         className="border-4 border-transparent hover:border-t-white text-white font-bold p-[15px] me-1 nav"
       >
-        <i className="ri-discuss-fill md:hidden text-2xl"></i>
+        <i className="ri-discuss-fill md:hidden text-xl"></i>
         <li className="hidden md:block">Discussion panel</li>
       </NavLink>
       {!isLecture && (
@@ -44,7 +44,7 @@ const SingleClassHeaderComponent = () => {
           to={`/class/${classId}/works`}
           className="border-4 border-transparent hover:border-t-white text-white font-bold p-[15px] me-1 nav"
         >
-          <i className="ri-calendar-todo-fill md:hidden text-2xl"></i>
+          <i className="ri-calendar-todo-fill md:hidden text-xl"></i>
           <li className="hidden md:block">Class work</li>
         </NavLink>
       )}
@@ -54,7 +54,7 @@ const SingleClassHeaderComponent = () => {
           to={`/class/${classId}/dashboard/`}
           className="border-4 border-transparent hover:border-t-white text-white font-bold p-[15px] me-1 nav"
         >
-          <i className="ri-dashboard-fill md:hidden text-2xl"></i>
+          <i className="ri-dashboard-fill md:hidden text-xl"></i>
           <li className="hidden md:block">Dashboard</li>
         </NavLink>
       )}
@@ -63,7 +63,7 @@ const SingleClassHeaderComponent = () => {
           to={`/class/${classId}/chat-mate/`}
           className="border-4 border-transparent  hover:border-t-white text-white font-bold p-[15px] me-1 nav"
         >
-          <i className="ri-chat-private-fill md:hidden text-2xl"></i>
+          <i className="ri-chat-private-fill md:hidden text-xl"></i>
           <li className="hidden md:block">Chat</li>
         </NavLink>
       )}
@@ -72,7 +72,7 @@ const SingleClassHeaderComponent = () => {
           to={`/meet`}
           className="border-4 border-transparent hover:border-t-white text-white font-bold p-[15px] me-1 nav"
         >
-          <i className="ri-vidicon-fill md:hidden text-2xl"></i>
+          <i className="ri-vidicon-fill md:hidden text-xl"></i>
           <li className="hidden md:block">Meet up</li>
         </NavLink>
       )}
@@ -80,7 +80,7 @@ const SingleClassHeaderComponent = () => {
         className="border-4 border-transparent hover:border-t-white text-white font-bold p-[15px] me-1 nav"
         onClick={handleSlide}
       >
-        <i className="ri-team-fill md:hidden text-2xl"></i>
+        <i className="ri-team-fill md:hidden text-xl"></i>
         <li className="hidden md:block">People</li>
       </button>
     </ul>
