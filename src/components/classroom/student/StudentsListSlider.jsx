cabad0e3ebc47bba3790d4b3132680fb228537b0
Ragesh-A <ragesha2017@gmail.com>
2023-06-07 12:42:22 +0530

    students filter

--- src/components/classroom/student/StudentsListSlider.jsx
+++ src/components/classroom/student/StudentsListSlider.jsx
@@ -1,30 +1,56 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import Shimmer from '../../common/Shimmer';
 
 const StudentListSlider = () => {
+  const { currentClass } = useSelector((store) => store.classes);
+  const [active, setActive] = useState(true);
+  const [filteredUsers, setFilteredUsers] = useState(currentClass?.students);
 
-  const { currentClass } = useSelector(store=>store.classes)
-  const [active, setActive] = useState(true)
-
-  const students = currentClass?.students
-  const { peopleSlide } = useSelector(store=>store.ui)
   useEffect(()=>{
-    setActive(active ? false : true)
-  }, [peopleSlide])
+    setFilteredUsers(currentClass?.students)
+  }, [currentClass])
+
+  const filterUser = (e) => {
+    const search = e.target.value;
+    const filtered = currentClass.students.filter((student) => {
+      return student.name.includes(search);
+    });
+    setFilteredUsers(filtered);
+  };
+  
+
+  const { peopleSlide } = useSelector((store) => store.ui);
+  useEffect(() => {
+    setActive(active ? false : true);
+  }, [peopleSlide]);
 
   return (
-    <div className={`fixed z-[3] ${active ? 'right-0' : 'right-[-100%]'} transitions w-80 max-w-full bg-tileColor h-full border-2 border-white shadow rounded-s-md p-5`}>
-      <div className="rounded py-2 px-3 border-2 border-white shadow-inner shadow-slate-300 bg-slate-100 flex items-center gap-2">
-        <i className="fa-solid fa-magnifying-glass text-textColor"></i>
-        <input type="text" name="name" id="name" className="w-full outline-none bg-transparent"/>
-      </div>
-      <ul className="flex flex-col gap-2">
-       {students && students.map(student=>(
-        <li className="p-1 border-2 border-transparent border-b-white" key={student?._id}>{student?.name}</li>
-       ))}
-      </ul>
+    <div
+      className={`fixed z-[3] ${
+        active ? 'right-0' : 'right-[-100%]'
+      } transitions w-80 max-w-full bg-tileColor h-full border-2 border-white shadow rounded-s-md p-5`}
+    >
+      {filteredUsers ? (
+        <>
+          <div className="rounded py-2 px-3 border-2 border-white shadow-inner shadow-slate-300 bg-slate-100 flex items-center gap-2">
+            <i className="fa-solid fa-magnifying-glass text-textColor"></i>
+            <input type="text" name="name" id="name" className="w-full outline-none bg-transparent" onChange={filterUser}/>
+          </div>
+          <ul className="flex flex-col gap-2">
+            {filteredUsers &&
+              filteredUsers.map((student) => (
+                <li className="p-1 border-2 border-transparent border-b-white" key={student?._id}>
+                  {student?.name}
+                </li>
+              ))}
+          </ul>
+        </>
+      ) : (
+        <Shimmer count={3}/>
+      )}
     </div>
-  )
+  );
 };
 
 export default StudentListSlider;
