450cb6cb187147e7ae2fddc3fa249798218f60fc
Ragesh-A <ragesha2017@gmail.com>
2023-06-28 14:46:15 +0530

    constants

---
+++ src/constant/constant.js
@@ -0,0 +1,28 @@
+export const BASE_URL = 'http://localhost:8000/api';
+
+export const BASIC_FEATURES = [
+  { class: 'fa-solid fa-people-roof', name: 'class' },
+  { class: 'fa-solid fa-tv', name: 'Video conferencing' },
+  { class: 'fa-solid fa-message', name: 'Discussions' },
+  { class: 'fa-solid fa-photo-film', name: 'materials sharing' },
+  { class: 'fa-solid fa-clipboard-list', name: 'assignments and exams' },
+  { class: 'fa-solid fa-table-list', name: 'Attendance tracking' },
+];
+export const PREMIUM_FEATURES = [
+  { class: 'fa-solid fa-people-roof', name: 'Unlimited classes' },
+  { class: 'fa-solid fa-chart-line', name: 'Track Progress' },
+  { class: 'fa-solid fa-message', name: 'Private and group chat' },
+  { class: 'fa-regular fa-circle-dot', name: 'Class recording' },
+  { class: 'fa-solid fa-bell', name: 'Notifications' },
+  { class: 'fa-solid fa-square-poll-vertical', name: 'Analytics and report' },
+];
+
+export const IP = 'http://localhost:3000';
+
+export const IMAGE_PATH = 'http://localhost:8000/images/';
+
+export const CHAT_SOCKET_IP = 'http://localhost:8000';
+export const MEETUP_SOCKET_IP = 'http://localhost:9090';
+
+export const ZEGO_SERVER_SECRET = '64772d6e1d1fd8bed531f0e9b6fb0659';
+export const zegoAppId = 421812598;
